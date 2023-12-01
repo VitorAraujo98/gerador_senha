cc484a48e5de1ac3c134bee26f5eb7f69b9f41c9
VitorAraujo98 <joaonikito98@gmail.com>
2023-12-01 16:41:48 -0300

    Parte lógica

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-console.log("text")
+let valorElement = document.querySelector("#valor");
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let passwordElement = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#container-password")
+
+
+
+let charset = "abcdfghijklmnopqrsty1234!@#$abc123456789";
+let novaSenha = "";
+
+
+valorElement.innerHTML = sliderElement.value;
+
+
+slider.oninput = function(){
+    valorElement.innerHTML = this.value;
+}
+
+
+function generatePassword(){
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+}
